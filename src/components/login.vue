17ab858b3ee7b53e5a38c34329436a7d4bfb0a33
Drozerah <drozerah@drozerah.user>
2018-09-27 17:54:40 +0200

    feat: add signIn method - signIn firebase user with email & password

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="login">
         <h3>Sign In</h3>
-        <input type="text" placeholder="Email"><br />
-        <input type="password" placeholder="password"><br />
-        <button v-on:click='login'>Connection</button>      
+        <input type="text" v-model="email" placeholder="Email"><br />
+        <input type="password" v-model="password" placeholder="password"><br />
+        <button @click='SignIn'>Connection</button>      
         <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
     </div>
 
 </template>
 
 <script>
+    import firebase from 'firebase'   
     export default {
         name: 'login',
         data: function () {
-            return {}
+            return {
+               email: '',
+               password: '' 
+            }
         },
         methods: {
             login: function () {
                 this.$router.replace('hello')
+            },
+            SignIn: function (){       
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+                    function (onResolve) {
+                        alert('Well done ! You are now connected')
+                    },
+                    function (error) {
+                        alert('Oops. ' + error.message)
+                    }
+                )
             }
         }
     }
