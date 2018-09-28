e06124940767d052e48935a66bcc98077321f6d6
Drozerah <drozerah@drozerah.user>
2018-09-28 18:24:36 +0200

    feat: navigation guard - add meta field to /hello route

--- src/router/index.js
+++ src/router/index.js
@@ -17,11 +17,6 @@ export default new Router({
       redirect: '/login', // redirect to the login path  
     },
     {
-      path: '/hello', // set the URL the user will visit if logged in
-      name: 'Hello', // use this name as a shortcut in links
-      component: Hello // load the Hello component
-    },
-    {
       path: '/login', // set the URL where the user to log in
       name: 'Login', // use this name as a shortcut in links
       component: Login // load the Hello component
@@ -30,6 +25,14 @@ export default new Router({
       path: '/sign-up', // set the URL where the user to sign up
       name: 'SignUp', // use this name as a shortcut in links
       component: SignUp // load the SignUp component
+    },
+    {
+      path: '/hello', // set the URL the user will visit if logged in
+      name: 'Hello', // use this name as a shortcut in links
+      component: Hello, // load the Hello component
+      meta: {
+        requiresAuth: true // meta field
+      }
     }
   ]
 })
