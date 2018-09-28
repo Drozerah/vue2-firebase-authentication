27eaebe9c8fec82e42cef929f1146c87fcbd714d
Drozerah <drozerah@drozerah.user>
2018-09-28 16:40:00 +0200

    refactor: replace the callback functions of the SignUp function by ES6 arrow functions

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,61 +1,59 @@
 <template>
-  <div class="sign-up">
-    <p>Let's create a new account !</p>
-    <input type="text" v-model='email' placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click='signUp'>Sign Up</button>
-    <span>or go back to <router-link to="/login">login</router-link>.</span>
-  </div>
+    <div class="sign-up">
+        <p>Let's create a new account !</p>
+        <input type="text" v-model='email' placeholder="Email"><br>
+        <input type="password" v-model="password" placeholder="Password"><br>
+        <button @click='signUp'>Sign Up</button>
+        <span>or go back to <router-link to="/login">login</router-link>.</span>
+    </div>
 </template>
 
 <script>
+    import firebase from "firebase";
 
-    import firebase from 'firebase'
     export default {
-    name: 'signUp',
-    data: function() {
-      return {
-          email:'',
-          password:''
-      }
-    },
-    methods: {
-            signUp: function(){
-
-                    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createUserWithEmailAndPassword
-                    firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
-                        function (onResolve) {
-                            // reset input fields
-                            alert('Your account has been created !')
-
-                        },
-                        function (error) {
-                            alert('Oops. ' + error.message) 
-                        }
-                    )
-
+        name: "signUp",
+        data: function() {
+            return {
+                email: "",
+                password: ""
+            };
+        },
+        methods: {
+            signUp: function() {
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(
+                (onResolve) => {
+                    alert("Your account has been created !");
+                },
+                (error) => {
+                    alert("Oops. " + error.message);
                 }
-            }         
-    }
+                );
+            }
+        }
+    };
 </script>
 
 <style scoped>
-  .signUp {
-    margin-top: 40px;
-  }
-  input {
-    margin: 10px 0;
-    width: 20%;
-    padding: 15px;
-  }
-  button {
-    margin-top: 10px;
-    width: auto;
-    cursor: pointer;
-  }
-  span {
-    display: block;
-    margin-top: 20px;
-    font-size: 11px;
-  }
+.signUp {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 10px;
+  width: auto;
+  cursor: pointer;
+}
+span {
+  display: block;
+  margin-top: 20px;
+  font-size: 11px;
+}
 </style>
